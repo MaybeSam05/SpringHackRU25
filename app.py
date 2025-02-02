fa69db10354d9a3a86b4189885790d888834ba97
Samarth Verma <verma.samarth05@gmail.com>
2025-02-02 03:35:42 -0500

    Beautified confirm.html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from  main import get_ingredients, get_website_data, get_images, clean_data, mergeDataImage
 import json
 import os
+import sys
 
 app = Flask(__name__)
 
@@ -14,14 +15,14 @@ def submit():
     final_data = []
     user_input = request.form.get("user-input") # hamburger buns
     ingredients = get_ingredients(user_input) # ["hamburger", "buns"]
-    
+
     for ingredient in ingredients:
         website = get_website_data(ingredient)
         images = get_images(ingredient)
         individual_data = mergeDataImage(website, images) # [["hamburger", "10.99", img], ["buns", "2.99", img]]
         final_data.append(individual_data)
 
-    return render_template('confirm.html', data=final_data, ingredients=ingredients)
+    return render_template('confirm.html', food=user_input,data=final_data, ingredients=ingredients)
 
 if __name__ == '__main__':
     app.run(debug=True)
